13c53248a0de040a455ce41c26a3d7db3fc46353
StoneWorker <newdaynewlook@126.com>
2019-07-06 14:55:21 +0800

    Add headers for angular which is used for webapi filter.

--- angular-vote-system/src/app/vote.service.ts
+++ angular-vote-system/src/app/vote.service.ts
@@ -5,10 +5,13 @@ import { User } from './user';
 import { VoteItem } from './vote-item';
 import { Observable } from 'rxjs';
 import { Md5} from "ts-md5/dist/md5";
+import { NullAstVisitor } from '@angular/compiler';
 
+/*
 const httpOptions = {
   headers: new HttpHeaders({'Content-Type': 'application/json'})
 };
+*/
 
 @Injectable()
 export class VoteService implements CanActivate{
@@ -21,6 +24,13 @@ export class VoteService implements CanActivate{
   //userIndex: number;  
   //Base server path
   baseServerPath: string ="http://localhost:53604/";
+
+  key: string = ':!123@321008'; 
+  userName:string = this.user!=null?this.user.name:'notLogin';  
+  token: string = Md5.hashStr( this.key+ this.userName).toString();    
+  httpOptions = {
+    headers :new HttpHeaders({'Content-Type': 'application/json'}).set('user',this.userName).set('token',this.token)  
+  };
 
   canActivate(route: ActivatedRouteSnapshot,state: RouterStateSnapshot): boolean {
     console.log('VoteService.canActivate method called');
@@ -54,13 +64,14 @@ export class VoteService implements CanActivate{
 */
 
   checkLogin(userName: string, password: string): boolean{
-    console.log(Md5.hashStr(password)); 
+    console.log(Md5.hashStr(password));     
     this.getLoginResult([userName, Md5.hashStr(password).toString(),"checkUser"]).subscribe(user =>{                 
         if(user!==null){  
           console.log('success login');     
           this.isLoggedIn = true;     
           this.user = user;          
           this.updateSessionStorage(this.user,this.isLoggedIn);
+          this.updateHeaders();
           this.router.navigate([this.redirectUrl]);
           console.log(this.user);
           return true;
@@ -72,6 +83,22 @@ export class VoteService implements CanActivate{
     return false; 
   }
 
+  updateHeaders(){
+    if(this.user != null){
+      this.userName = this.user.name;
+    }
+    else{
+      this.userName = 'notLogin';
+    }
+    this.token = Md5.hashStr( this.key+ this.userName).toString(); 
+    this.httpOptions = {
+      headers :new HttpHeaders({'Content-Type': 'application/json'}).set('user',this.userName).set('token',this.token)  
+      };
+    console.log(this.httpOptions); 
+    console.log(this.token); 
+    console.log(this.userName);
+  }
+
   updateSessionStorage(user:User,isLoggedIn?:boolean){
     sessionStorage.setItem('user', JSON.stringify(user));
     if(isLoggedIn!=null)
@@ -80,50 +107,47 @@ export class VoteService implements CanActivate{
   
   getLoginResult(user:string[]):Observable<any>{  
     const url=`${this.baseServerPath}api/users`;
-    return this.http.post<any>(url, user, httpOptions);
+    return this.http.post<any>(url, user, this.httpOptions);
   }
 
   register(user:string[]):Observable<boolean>{  
     const url=`${this.baseServerPath}api/users`;
-    return this.http.post<boolean>(url, user, httpOptions);
+    return this.http.post<boolean>(url, user, this.httpOptions);
   }
 
   getUsers():Observable<User[]>{
     const url=`${this.baseServerPath}api/users`;
-    return this.http.get<User[]>(url);
+    return this.http.get<User[]>(url,this.httpOptions);
   }
 
   getUsersByName(name: string):Observable<User[]>{
     const url=`${this.baseServerPath}api/users/?name=${name}`;
-    return this.http.get<User[]>(url);
+    return this.http.get<User[]>(url,this.httpOptions);
   }
 
   updateUser(user: User): Observable<any>{
-    return this.http.put(`${this.baseServerPath}api/users`, user, httpOptions); 
+    return this.http.put(`${this.baseServerPath}api/users`, user, this.httpOptions); 
   }
 
   getvoteItems(name: string):Observable<VoteItem[]>{
-
     const  url=`${this.baseServerPath}api/${name}`;
-    return this.http.get<VoteItem[]>(url);
+    return this.http.get<VoteItem[]>(url,this.httpOptions);
   }
 
   getvoteItem(name: string, id: number):Observable<VoteItem>{
     const  url=`${this.baseServerPath}api/${name}/${id}`;
-    return this.http.get<VoteItem>(url);
+    return this.http.get<VoteItem>(url,this.httpOptions);
   }
 
   addvoteItem(name: string, voteItem: VoteItem):Observable<VoteItem>{
     const  url=`${this.baseServerPath}api/${name}`;
-    return this.http.post<VoteItem>(url, voteItem, httpOptions);
+    return this.http.post<VoteItem>(url, voteItem, this.httpOptions);
   }
 
   updatevoteItem(name: string, voteItem: VoteItem):Observable<any>{
     const  url=`${this.baseServerPath}api/${name}`;
-    return this.http.put<VoteItem>(url, voteItem, httpOptions);
+    return this.http.put<VoteItem>(url, voteItem, this.httpOptions);
   }
-
-
 
 
   logout(){
@@ -131,6 +155,7 @@ export class VoteService implements CanActivate{
     this.user = null;
     sessionStorage.removeItem('user');
     sessionStorage.removeItem('isLoggedIn'); 
+    this.updateHeaders();
     this.router.navigate(['/login']);
   }
 
